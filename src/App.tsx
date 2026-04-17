968854fad7e3e962db6157e7ecc9ea08f1864c49
Igor Josiel <igorjosielvitalino@gmail.com>
2026-04-17 00:16:40 -0300

    feat: Adiciona a estrutura de roteamento das primeiras páginas.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import "./styles/global.css";
 
 import { Provider as ReduxProvider } from "react-redux";
+import { Routes, Route } from "react-router";
 
 import { store } from "./store";
-/* import Player from "./pages/Player"; */
+import Player from "./pages/Player";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <ReduxProvider store={store}>
-      <Dashboard />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="teste" element={<Player />} />
+      </Routes>
     </ReduxProvider>
   );
 }
